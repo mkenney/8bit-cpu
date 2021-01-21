b535870f566e3f7bf2702606a14a1b3461578f70
Michael Kenney <mkenney@webbedlam.com>
2021-01-21 14:31:12 -0700

    save

--- compiler/add.asm
+++ compiler/add.asm
@@ -2,7 +2,7 @@
 $d1 0x1C   # hex 28
 $d2 0b1110 # bin 14
 
-    LDAV    $d1 # set register 0 to 28
-    LDXV    $d2 # set register 1 to 14
-    ADDX        # add register 0 (always) + register 1, store result in register 0 (always)
-    OUTA        # copy register 0 to the output register
+    LDAV    $d1 # set register A to 28
+    LDXV    $d2 # set register X to 14
+    ADDX        # add register A (always) + register X, store result in register A (always)
+    OUTA        # copy register A to the output register
